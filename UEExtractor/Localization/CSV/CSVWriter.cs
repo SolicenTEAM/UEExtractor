1993c6dc9d821aa754034f6ac339047bc641714e
DenisSolicen <DenisSolicen@yandex.ru>
2025-04-28 02:56:58 +0300

    Add small CSV for skipped_lines.csv func.

---
+++ UEExtractor/Localization/CSV/CSVWriter.cs
@@ -0,0 +1,25 @@
+﻿using Solicen.Localization.UE4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CSVWriter
+{
+    public string FilePath { get; }
+    public CSVWriter(string filePath)
+    {
+        if (File.Exists(filePath))
+            File.Delete(filePath);
+
+        FilePath = filePath;
+    }
+
+    public void WriteLine(string text)
+    {
+        if (FilePath == string.Empty) return;
+        text = LocresHelper.EscapeKey(text);
+        File.AppendText(FilePath).WriteLine(text+"\n");
+    }
+}
