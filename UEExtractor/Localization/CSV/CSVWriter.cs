4647c1cf5b8bf268ea21ad0b660608f703c38010
DenisSolicen <DenisSolicen@yandex.ru>
2025-09-09 15:58:28 +0300

    Update CSVWriter.cs

--- UEExtractor/Localization/CSV/CSVWriter.cs
+++ UEExtractor/Localization/CSV/CSVWriter.cs
@@ -7,19 +7,23 @@ using System.Threading.Tasks;
 
 public class CSVWriter
 {
+    public StreamWriter Writer;
     public string FilePath { get; }
     public CSVWriter(string filePath)
     {
+        if (filePath == string.Empty) return;
         if (File.Exists(filePath))
             File.Delete(filePath);
 
         FilePath = filePath;
+        Writer = new StreamWriter(FilePath, true);
     }
 
     public void WriteLine(string text)
     {
         if (FilePath == string.Empty) return;
         text = LocresHelper.EscapeKey(text);
-        File.AppendText(FilePath).WriteLine(text+"\n");
+        Writer.WriteLine(text + "\n");
+        //File.AppendText(FilePath).WriteLine(text + "\n");
     }
 }
