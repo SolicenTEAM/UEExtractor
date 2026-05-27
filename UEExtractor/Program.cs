6709952d819cc489fd58448505556a6486999686
DenisSolicen <DenisSolicen@yandex.ru>
2026-05-27 20:13:00 +0300

    Brandnig returned to Solicen

--- UEExtractor/Program.cs
+++ UEExtractor/Program.cs
@@ -6,14 +6,13 @@ namespace UEExtractor
 {
     class Program
     {
-        const string title = $"UEExtractor VER : https://github.com/Nuked88/UEExtractor";
+        const string title = $"UEExtractor VER : https://github.com/SolicenTEAM/UEExtractor";
         static void Main(string[] args)
         {
             var Version = $"{Assembly.GetExecutingAssembly().GetName().Version}";
-            Console.Title = "UEExtractor • Nuked88";
+            Console.Title = "UEExtractor • Solicen";
             Solicen.CLI.Console.WriteLine($"{title.Replace("VER", Version)}", ConsoleColor.Cyan);
-            Solicen.CLI.Console.WriteLine("Fork maintained by: Nuked88 : https://github.com/Nuked88/UEExtractor", ConsoleColor.Cyan);
-            Solicen.CLI.Console.WriteLine("Original author: Denis Solicen : https://github.com/DenisSolicen", ConsoleColor.DarkCyan);
+            Solicen.CLI.Console.WriteLine("Author: Denis Solicen : https://github.com/DenisSolicen", ConsoleColor.Cyan);
             Solicen.CLI.Console.WriteLine("\nThis cool tool will collect and extract the .locres file from the resources of your Unreal Engine game folder.");
             Solicen.CLI.Console.WriteLine("Usage: Drag and drop whole the game folder that contain unreal archives: '.pak' or '.ucas' files.");
             Thread.Sleep(100);
@@ -31,8 +30,7 @@ namespace UEExtractor
 
             Solicen.CLI.Console.WriteLine($"Operation completed in: {timeTaken.TotalSeconds} seconds");
             Solicen.CLI.Console.WriteLine("\nIf this fork was useful to you, please put a star on its GitHub page, thank you!", ConsoleColor.Yellow);
-            Solicen.CLI.Console.WriteLine("Support Nuked88: https://ko-fi.com/nuked", ConsoleColor.Yellow);
-            Solicen.CLI.Console.WriteLine("Support original author Solicen: https://boosty.to/denissolicen/donate", ConsoleColor.DarkYellow);
+            Solicen.CLI.Console.WriteLine("Toss a coin: https://boosty.to/denissolicen/donate", ConsoleColor.Yellow);
             Console.ReadLine();
         }
     }
